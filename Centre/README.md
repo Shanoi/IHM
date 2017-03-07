b716d3fc9909680fd6c9b1e1d37091c516c26392
Jeremy Junac <jeremy.junac@etu.unice.fr>
2017-03-07 22:02:19 +0100

    Add controllers

---
+++ Centre/README.md
@@ -0,0 +1,15 @@
+## Question à poser
+
+* Doit-on faire le add/remove et le changement d'ordre ?
+* Traduction ?
+* ClassLoader enum ?
+
+
+
+## TODO
+
+* Controller administration
+* Controller index
+* Renommer le css
+* Pour le changement de scene:
+    http://stackoverflow.com/questions/13003323/javafx-how-to-change-stage
--- Centre/src/main/java/fr/unice/polytech/a/ihm/g2c/MainApp.java
+++ Centre/src/main/java/fr/unice/polytech/a/ihm/g2c/MainApp.java
@@ -1,36 +1,59 @@
 package fr.unice.polytech.a.ihm.g2c;
 
+import fr.unice.polytech.a.ihm.g2c.common.AppScene;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+import static fr.unice.polytech.a.ihm.g2c.common.AppScene.*;
 
 public class MainApp extends Application {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    public static final int HEIGHT = 1080;
+    public static final int WIDTH = 1920;
+    
+    private static final Logger logger = LogManager.getLogger(MainApp.class);
+
 
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
     public void start(Stage stage) throws Exception {
+        initScene();
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
-        scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Hello JavaFX and Maven");
-        stage.setScene(scene);
+        stage.setTitle("Cap Sophia");
+        stage.setScene(INDEX.getScene());
+        stage.setMaximized(true);
+        stage.setFullScreen(true);
         stage.show();
+    }
+
+    private void initScene() {
+        for (AppScene appScene: values()) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                Parent rootNode = loader.load(getClass().getResourceAsStream(appScene.getFxmlFile()));
+                Scene scene = new Scene(rootNode, WIDTH, HEIGHT);
+                scene.getStylesheets().add(appScene.getCssFile());
+                appScene.setScene(scene);
+
+
+            } catch (IOException e) {
+                logger.error(e);
+            }
+        }
     }
 }
