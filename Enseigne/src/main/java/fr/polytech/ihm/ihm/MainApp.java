0214db763286b8b387200e29e97cb20a1bf366dd
dallanoe <enzo.dalla-nora@etu.unice.fr>
2017-03-11 19:51:19 +0100

    Admin panel maintenant capable de :
    - Ajouter un objet

--- Enseigne/src/main/java/fr/polytech/ihm/ihm/MainApp.java
+++ Enseigne/src/main/java/fr/polytech/ihm/ihm/MainApp.java
@@ -24,16 +24,18 @@ public class MainApp extends Application {
     }
 
     public void start(Stage stage) throws Exception {
+        int widthAdmin = 800;
+        int heightAdmin = 533;
 
         log.info("Starting Hello JavaFX and Maven demonstration application");
 
-        String fxmlFile = "/fxml/MainPage.fxml";
+        String fxmlFile = "/fxml/AdminPanel.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 1920, 1080);
+        Scene scene = new Scene(rootNode, widthAdmin, heightAdmin);
         scene.getStylesheets().add("/styles/styles.css");
 
         stage.setTitle("To Be Or To Have - Admin panel");
