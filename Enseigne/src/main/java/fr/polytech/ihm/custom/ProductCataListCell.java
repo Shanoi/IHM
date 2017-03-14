4e71870044f80982923e7151ed99cb62fccd2af0
Alexandre Redon <alexandre.redon@etu.unice.fr>
2017-03-14 20:31:08 +0100

    extension alex

--- Enseigne/src/main/java/fr/polytech/ihm/custom/ProductCataListCell.java
+++ Enseigne/src/main/java/fr/polytech/ihm/custom/ProductCataListCell.java
@@ -13,7 +13,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 
 /**
  *
